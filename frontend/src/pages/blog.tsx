ce2b6551d66f2710d6f2602bf74838a901ae6d3b
juan <juan2007@gmail.com>
2024-08-13 16:50:20 -0600

    blog page and modifications to the head

---
+++ frontend/src/pages/blog.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function BlogPage(){
+    return(
+        <>
+        <Head>
+        <title>blog page</title>
+        <meta name="description" content="my blog space" />
+        
+      </Head>
+        <div>
+            Hello, BLog
+        </div>
+        </>
+    )
+}
