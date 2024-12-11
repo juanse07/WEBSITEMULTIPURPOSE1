f6f6d41ad84840e62030f15039c9b799bf9f22be
juan <juan2007@gmail.com>
2024-12-10 19:37:37 -0700

    nextconfig changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,  // Add this line to bypass Next.js image optimization
-    domains: ['localhost'],
+    domains: ['localhost',"api.elegopro.com"],
     deviceSizes: [320, 420, 768, 1024, 1200],
   },
 }
