8581966af1b4e3419a5e04165b8e48209aae0c72
juan <juan2007@gmail.com>
2024-12-19 16:06:43 -0700

    index font change

--- frontend/src/components/homePage/ServiceSection.tsx
+++ frontend/src/components/homePage/ServiceSection.tsx
@@ -75,7 +75,7 @@ const ServiceSection: React.FC<ServiceSectionProps> = ({
           >
           <Col >
             <div >
-              <span >{category}</span>
+              <span className={styles.category} >{category}</span>
               <h2 className={styles.ServiceTitle} >{title}</h2>
               <p className={styles.ServiceDescription} >{description}</p>
               <div className={styles.serviceActions} >
@@ -147,67 +147,3 @@ const ServiceSection: React.FC<ServiceSectionProps> = ({
 export default ServiceSection;
 
 
-// import Image from 'next/image';
-// import React from 'react';
-// import { Button, Col, Container, Row } from 'react-bootstrap';
-// import { FaComments } from 'react-icons/fa';
-// import { FiMail } from 'react-icons/fi';
-
-// interface ServiceSectionProps {
-//   category: string;
-//   title: string;
-//   description: string;
-//   price: number;
-//   imageUrl: string;
-//   imagePosition?: 'left' | 'right';
-// }
-
-// const ServiceSection: React.FC<ServiceSectionProps> = ({
-//   category,
-//   title,
-//   description,
-//   price,
-//   imageUrl,
-//   imagePosition = 'right'
-// }) => {
-//   return (
-//     <section className="service-section py-3">
-//       <Container className="service-container">
-//         <Row className={`align-items-center service-row ${imagePosition === 'left' ? 'flex-row-reverse' : ''}`}>
-//           <Col md={8} className="service-content">
-//             <div className="pe-md-4">
-//               <span className="service-category">{category}</span>
-//               <h2 className="service-title">{title}</h2>
-//               <p className="service-description">{description}</p>
-//               <div className="d-flex gap-3 align-items-center">
-//                 <Button 
-//                   variant="outline-primary"  
-//                   //className="service-button"
-//                 >
-//                   Book {category.toLowerCase()} from: ${price}
-//                 </Button>
-//                 <a href="#" className="service-icon">
-//                   <FaComments size={24} />
-//                 </a>
-//                 <a href="#" className="service-icon">
-//                   <FiMail size={24} />
-//                 </a>
-//               </div>
-//             </div>
-//           </Col>
-//           <Col md={4} className= "service-image-col" >
-//             <Image 
-//               src={imageUrl} 
-//               alt={title}
-//               width={400}
-//               height={350} 
-//               className="service-image"
-//             />
-//           </Col>
-//         </Row>
-//       </Container>
-//     </section>
-//   );
-// };
-
-// export default ServiceSection;
